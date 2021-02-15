0fe7d0a380a3e1d11abda772ceffeca0a9e89cf3
Jos Dehaes <jos.dehaes@gmail.com>
2021-02-15 20:37:25 +0100

    add start full screen option

--- ACVM/VMConfigVC.swift
+++ ACVM/VMConfigVC.swift
@@ -30,7 +30,8 @@ class VMConfigVC: NSViewController, FileDropViewDelegate {
     @IBOutlet weak var unhideMousePointer: NSButton!
     @IBOutlet weak var arm64RadioButton: NSButton!
     @IBOutlet weak var x64RadioButton: NSButton!
-    
+    @IBOutlet weak var startFullScreen: NSButton!
+
     // Disk Pane
     @IBOutlet weak var mainImage: FileDropView!
     @IBOutlet weak var useVirtIOForDisk: NSButton!
@@ -164,6 +165,12 @@ class VMConfigVC: NSViewController, FileDropViewDelegate {
             unhideMousePointer.state = .off
         }
         
+        if virtMachine.config.startFullScreen {
+            startFullScreen.state = .on
+        } else {
+            startFullScreen.state = .off
+        }
+
         if virtMachine.config.mainImageUseVirtIO {
             useVirtIOForDisk.state = .on
         } else {
@@ -255,6 +262,12 @@ class VMConfigVC: NSViewController, FileDropViewDelegate {
                     virtMachine.config.unhideMousePointer = true
                 }
                 
+                if startFullScreen.state == .off {
+                    virtMachine.config.startFullScreen = false
+                } else {
+                    virtMachine.config.startFullScreen = true
+                }
+
                 if useVirtIOForDisk.state == .off {
                     virtMachine.config.mainImageUseVirtIO = false
                 } else {
